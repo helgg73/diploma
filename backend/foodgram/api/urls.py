8b0f05a294c8742a3acc4abf8283489964673535
Олег Дядьков <dyadkov@gmail.com>
2023-07-30 16:58:36 +0500

    First settings

---
+++ backend/foodgram/api/urls.py
@@ -0,0 +1,11 @@
+"""foodgram.api URL Configuration
+"""
+from django.urls import path, include
+from djoser import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+
+]
